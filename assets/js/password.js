daf0bdf47ebcc3b5d4bb5fb79b3b4314741c3c81
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-05-02 09:40:34 +0600

    Create password.js

---
+++ assets/js/password.js
@@ -0,0 +1,7 @@
+<script type="text/javascript"> 
+var password = 'RAB8'
+password=prompt('Please enter the password to enter this page:','');
+if (password != 'RAB8') {
+location.href='https://masumbillah6778.github.io/best-porn-site';
+}
+</script>
