abd1f460d26d0d5e11eb7bda2f0104cb3301d4f9
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-05-04 05:37:25 +0600

    Update password.js

--- assets/js/password.js
+++ assets/js/password.js
@@ -5,7 +5,7 @@ var Password ="1234";
 function passcheck(){
     if (document.getElementById('pass1').value != Password) {
         errorText.style.display = "block";
-        errorText.textContent = "ERROR! Password Not Match.";
+        errorText.textContent = "দুঃখিত! পাসওয়ার্ড মিলছে না।";
         return false;
     }
     if (document.getElementById('pass1').value == Password) {
