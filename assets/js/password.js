b472d6ba9e99d159fc2cce19daf59d30db979a2e
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-05-04 19:36:56 +0600

    Update password.js

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,11 +1,10 @@
-// by https://www.gxfreee.com/
 	
 const errorText = document.querySelector(".error-text");
 var Password ="1234";
 function passcheck(){
     if (document.getElementById('pass1').value != Password) {
         errorText.style.display = "block";
-        errorText.textContent = "দুঃখিত! পাসওয়ার্ড মিলছে না।";
+        errorText.textContent = "দুঃখিত! ভুল পাসওয়ার্ড।";
         return false;
     }
     if (document.getElementById('pass1').value == Password) {
@@ -13,4 +12,3 @@ function passcheck(){
     }
 
 }
-// by https://www.gxfreee.com/
